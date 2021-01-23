5552aecaa5f05c8636e50a4a998702fcc4e60116
phys-ken <cromartie24@icloud.com>
2021-01-24 01:52:36 +0900

    疑似正弦波を出力

--- 1HzAC.ino
+++ 1HzAC.ino
@@ -1,7 +1,7 @@
 //モータードライバーとして、DRV8835を使用
 //TODO:OLEDに表示する
 
-#define PIN_IN1 4
+#define PIN_IN1 3
 #define PIN_IN2 5
 #define PIN_VREF 3 // PWM
 const int INPUT_PIN = A0;
@@ -13,11 +13,31 @@ int state = 0;
 float fq = 1;
 float volvol;
 
+int setsize = 10;
+int sinw[10]; //180の約数じゃないとだめ,配列数はsetsizeとそろえる。
+
+int InductorU = 0;
+int InductorV = setsize / 2;
+int ang;
+int MODE;
+int mag;
+float d;
+float in1, in2;
+
 void setup()
 {
+
+  Serial.begin(9600);
+  Serial.println("start");
   pinMode(PIN_IN1, OUTPUT);
   pinMode(PIN_IN2, OUTPUT);
-  Serial.begin(9600);
+  //setsizeに分割したsinのテーブルを作成
+  mag = 170; //0～255の範囲で設定
+  int stepsize = 360 / setsize;
+  for (ang = 0; ang < 360; ang = ang + stepsize)
+  {
+    sinw[ang / stepsize] = mag * (sin(ang * 3.14 / 180));
+  }
 }
 
 void loop()
@@ -27,70 +47,56 @@ void loop()
   Serial.print("Volume: ");       // シリアルモニタに出力
   Serial.println(VOLUME);
 
-  fq = map(VOLUME, 0, 1023, 1, 10); //VOLUMEに合わせて振動数を1～10に変更
+  fq = map(VOLUME, 0, 1023, 1, 3); //VOLUMEに合わせて振動数を1～3に変更
+  Serial.print("freq[Hz]: ");
   Serial.println(fq);
+  if (fq >= 0.001)
+  {
+    d = 1.0 / (fq * setsize);
+    Serial.print("DelayTime[ms]: ");
+    Serial.println(d * 1000);
+  }
+  else if (fq < 0.001) //0で割らないように
+  {
+    d = 0.01;
+  }
 
-  //前進
-  Serial.println("forward!");
-  digitalWrite(PIN_IN1, HIGH);
-  digitalWrite(PIN_IN2, LOW);
-  delay(500 / fq);
+  //sinwが負のときには出力しない
+  if (sinw[InductorU] < 1)
+  {
+    in1 = 0;
+  }
+  else
+  {
+    in1 = sinw[InductorU];
+  }
 
-  //後進
-  Serial.println("back!");
-  // 逆回転
-  digitalWrite(PIN_IN1, LOW);
-  digitalWrite(PIN_IN2, HIGH);
-  delay(500 / fq);
+  if (sinw[InductorV] < 1)
+  {
+    in2 = 0;
+  }
+  else
+  {
+    in2 = sinw[InductorV];
+  }
+
+  //sinw[]で指定した強度で、PWM出力
+  analogWrite(PIN_IN1, in1);
+  analogWrite(PIN_IN2, in2);
+  InductorV = update_phase(InductorV);
+  InductorU = update_phase(InductorU);
+
+  //dだけ遅延する。
+  delay(d * 1000);
+
+  //delay(1000);//debug用
 }
 
-//void loop(){
-//
-//    // モーターの回転速度を中間にする
-//    analogWrite(PIN_VREF,127);
-//
-//    if(digitalRead(INPUT_PIN) == HIGH){ // 入力ピン(タクトスイッチ)の状態を読み取る
-//      Serial.println("forward!");
-//      // 回転
-//      digitalWrite(PIN_IN1,HIGH);
-//      digitalWrite(PIN_IN2,LOW);
-//    } else {
-//      Serial.println("back!");
-//      // 逆回転
-//      digitalWrite(PIN_IN1,LOW);
-//      digitalWrite(PIN_IN2,HIGH);
-//    }
-
-//
-//    // 回転
-//    digitalWrite(PIN_IN1,HIGH);
-//    digitalWrite(PIN_IN2,LOW);
-//    delay(5000);
-//
-//    // ブレーキ
-//    digitalWrite(PIN_IN1,HIGH);
-//    digitalWrite(PIN_IN2,HIGH);
-//
-//    // 逆回転
-//    digitalWrite(PIN_IN1,LOW);
-//    digitalWrite(PIN_IN2,HIGH);
-//    delay(5000);
-//
-//    // ブレーキ
-//    digitalWrite(PIN_IN1,HIGH);
-//    digitalWrite(PIN_IN2,HIGH);
-//    delay(2000);
-//
-//    // モーターの回転速度を最大にする
-//    analogWrite(PIN_VREF,255);
-//
-//    // 逆回転　
-//    digitalWrite(PIN_IN1,LOW);
-//    digitalWrite(PIN_IN2,HIGH);
-//    delay(5000);
-//
-//    // ストップ
-//    digitalWrite(PIN_IN1,LOW);
-//    digitalWrite(PIN_IN2,LOW);
-//    delay(2000);
-//}
+//inductorを進める
+int update_phase(int Inductor)
+{
+  Inductor = Inductor + 1;
+  if (Inductor >= setsize)
+    Inductor = 0;
+  return Inductor;
+}
--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # 1Hz 交流発振回路
-高校物理の実験教材です。[DRV8835](https://akizukidenshi.com/catalog/g/gK-10346/)を使って、指定した周波数で電源をスイッチングします。
+高校物理の実験教材です。[DRV8835](https://akizukidenshi.com/catalog/g/gK-10346/)を使って、疑似正弦波交流を作成します。  
+  
+. 参考にしたサイト
+  - [ArduinoでVVVFインバータ作ってみた](https://qiita.com/tueks3/items/b0e06d58cec6778cc54d)  
+  上記サイトでは30Hzが限界と書いてありましたが、私の環境では疑似正弦波の分割数を10個まで落としても3Hzまでしか出せませんでした。超格安のArduino nano互換機を使っているのが原因か？
